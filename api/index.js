ee2e7054484189cbe53c4b2050be1966b1967a0c
Leyton <leytoncespedes123@gmail.com>
2026-03-18 17:04:33 -0400

    corrigiendo errores

--- api/index.js
+++ api/index.js
@@ -7,6 +7,26 @@ const middlewares = jsonServer.defaults();
 
 app.use(middlewares);
 app.use(jsonServer.bodyParser);
+
+app.get("/:resource/:id", (req, res, next) => {
+	const { resource, id } = req.params;
+	const collection = router.db.get(resource);
+
+	if (!collection || !Array.isArray(collection.value())) {
+		return next();
+	}
+
+	const entity = collection.find((item) => String(item.id) === String(id)).value();
+
+	if (!entity) {
+		return res.status(404).json({
+			message: `El id ${id} no existe en ${resource}`
+		});
+	}
+
+	return next();
+});
+
 app.use(router);
 
 module.exports = app;
